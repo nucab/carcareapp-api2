94487dd24776ccce8ccc518f85b66a7e42d9fcac
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-02-12 06:35:01 +0800

    re-added pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : "carcarecapp-api",
+      script    : "./server/index.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      key  : "./ssh/id_rsa",
+      user : "carcareapp",
+      host : "138.197.108.171",
+      ref  : "origin/master",
+      repo : "https://github.com/noahjohn9259/carcareapp-api2.git",
+      path : "/home/carcareapp/public",
+      "post-deploy" : "nvm install && npm install && ~/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/public/source/ecosystem.config.js --env production"
+    }
+  }
+}
