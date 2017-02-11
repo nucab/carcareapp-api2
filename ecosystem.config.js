b7688fd012524b603055b7a95bc3a53f9258daa2
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-02-12 07:43:39 +0800

    update package conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,9 @@ module.exports = {
       name      : "carcarecapp-api",
       script    : "./server/index.js",
 	    env : {
+		    NODE_ENV: "development"
+	    },
+	    env_production : {
 		    NODE_ENV: "production"
 	    }
     }
@@ -27,7 +30,7 @@ module.exports = {
       ref  : "origin/master",
       repo : "https://github.com/noahjohn9259/carcareapp-api2.git",
       path : "/home/carcareapp/public",
-      "post-deploy" : "nvm install && npm install && ~/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/public/source/ecosystem.config.js --env production && npm run bootstrap"
+      "post-deploy" : "nvm install && npm install && NODE_ENV=production npm run bootstrap && ~/.nvm/versions/node/v6.9.4/bin/pm2 startOrRestart ~/public/source/ecosystem.config.js --env production"
     }
   }
 }
