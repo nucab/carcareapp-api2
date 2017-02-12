5da34c6297797dab723113b5b6ceebc0f939f15e
Noah John Ucab <noahjohn.ucab@gmail.com>
2017-02-13 03:34:48 +0800

    updated space

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ let app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+
+
 // parse application/json
 app.use(bodyParser.json());
 
